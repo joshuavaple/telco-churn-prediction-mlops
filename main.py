06b979e576a53067c8551a72f160ce9cb3ae49fd
joshuale@globalpsa.com <joshuale@globalpsa.com>
2024-12-11 05:56:27 +0000

    added note to sample main.py
    Noted about the need to clear states for changes in imported modules to take effect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.utils import generate_uuid4, generate_random_int
 
-
+# you may need to clear state to reflect changes in your modules
+# Run/Clear/Clear state and outputs
 print(generate_uuid4())
 print(generate_random_int())
