213575c75cae071d060c69b08aa843ba42c8ef29
joshuale@globalpsa.com <joshuale@globalpsa.com>
2024-12-17 08:14:21 +0000

    updated to test relative import

--- main.ipynb.py
+++ main.ipynb.py
@@ -1,6 +1,13 @@
 # Databricks notebook source
 from src.utils import generate_uuid4, generate_random_int
-
-
+# you may need to clear state to reflect changes in your modules
+# Run/Clear/Clear state and outputs
 print(generate_uuid4())
 print(generate_random_int())
+
+
+# COMMAND ----------
+
+from src.text_processing.text_processor import remove_non_alphanumeric
+
+print(remove_non_alphanumeric("Hello World!"))
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from src.utils import generate_uuid4, generate_random_int
+from src.text_processing.text_processor import remove_non_alphanumeric
+
 
 # you may need to clear state to reflect changes in your modules
 # Run/Clear/Clear state and outputs
 print(generate_uuid4())
 print(generate_random_int())
+print(remove_non_alphanumeric("Hello World!"))
